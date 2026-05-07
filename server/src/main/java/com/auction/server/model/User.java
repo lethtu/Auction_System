eddaec97c62e22a2c5e0af12957b4e1087b33e5b
tung lê <tung.leth07@gmail.com>
2026-05-07 18:04:18 +0700

    Fix lỗi không thể đăng ký, sửa logic ít nhất, tránh thay đổi, thêm test cho client

--- server/src/main/java/com/auction/server/model/User.java
+++ server/src/main/java/com/auction/server/model/User.java
@@ -1,16 +1,14 @@
 package com.auction.server.model;
 
-import java.time.LocalDate;
-import java.math.BigDecimal;
 import jakarta.persistence.*;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "users")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "role", discriminatorType = DiscriminatorType.STRING)
-public abstract class User {
-
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -19,46 +17,42 @@ public abstract class User {
     private String password;
     private String fullname;
     private String email;
-    private LocalDate dob;
-
-    @Column(name = "place_of_birth")
-    private String placeOfBirth;
-
+    private String dob;
+    private String place_of_birth;
+    private BigDecimal balance = BigDecimal.ZERO;
     @Column(name = "role", insertable = false, updatable = false)
     private String accountType;
-
-    private BigDecimal balance = BigDecimal.ZERO;
 
     public User() {}
 
     public void setId(Integer id) { this.id = id; }
+
     public void setUsername(String username) { this.username = username; }
+
     public void setPassword(String password) { this.password = password; }
+
     public void setFullname(String fullname) { this.fullname = fullname; }
+
     public void setEmail(String email) { this.email = email; }
-    public void setDob(LocalDate dob) { this.dob = dob; }
-    public void setPlaceOfBirth(String placeOfBirth) { this.placeOfBirth = placeOfBirth; }
+
+    public void setDob(String dob) { this.dob = dob; }
+
+    public void setPlace_of_birth(String place_of_birth) { this.place_of_birth = place_of_birth; }
+
     public void setBalance(BigDecimal balance) { this.balance = balance; }
 
     public Integer getId() { return id; }
-    public String getUsername() { return username; }
-
-    @JsonIgnore
-    public String getPassword() { return password; }
     public String getFullname() { return fullname; }
     public String getEmail() { return email; }
-    public LocalDate getDob() { return dob; }
-    public String getPlaceOfBirth() { return placeOfBirth; }
+    public String getDob() { return dob; }
+    public String getPlace_of_birth() { return place_of_birth; }
+    public String getUsername() { return username; }
+    public String getPassword() { return password; }
     public String getAccountType() { return accountType; }
     public BigDecimal getBalance() { return balance; }
 
     @Override
-    public String toString() {
-        return "User [" +
-                "ID=" + id +
-                ", Fullname='" + fullname + '\'' +
-                ", AccountType='" + accountType + '\'' +
-                ", Username='" + username + '\'' +
-                ']';
+    public String toString(){
+        return id + " " + fullname + " " + email + " " + dob + " " + place_of_birth + " " + username + " " + password;
     }
 }
